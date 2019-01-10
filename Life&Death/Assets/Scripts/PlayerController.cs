fdd6d0f58381cb192ae047f5d65d8d616d128e70
venstex <32637535+venstex@users.noreply.github.com>
2019-01-10 22:44:40 +0100

    first testscene with movement and rotation

---
+++ Life&Death/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float rotationSpeed = 5;
+
+    [SerializeField]
+    private float MovementSpeed = 0.2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal")* rotationSpeed;
+        float moveVertical = Input.GetAxis("Vertical") * MovementSpeed;
+        Debug.Log(moveHorizontal + " : " + moveVertical);
+
+        this.transform.Translate(0, 0, moveVertical);
+        this.transform.Rotate(0,moveHorizontal,0);
+
+        if (Input.GetKeyDown("space"))
+        {
+
+        }
+    }
+}
