e2318a11f2c39839c6ed9088d35158cbdde66f91
venstex <32637535+venstex@users.noreply.github.com>
2019-01-11 00:16:35 +0100

    working bounds for x and z

--- Life&Death/Assets/Scripts/PlayerController.cs
+++ Life&Death/Assets/Scripts/PlayerController.cs
@@ -9,8 +9,13 @@ public class PlayerController : MonoBehaviour
     private float rotationSpeed = 5;
 
     [SerializeField]
-    private float MovementSpeed = 0.2f;
+    private float movementSpeed = 0.2f;
 
+    [SerializeField]
+    private float LimitZ = 5f;
+
+    [SerializeField]
+    private float LimitX = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +27,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal")* rotationSpeed;
-        float moveVertical = Input.GetAxis("Vertical") * MovementSpeed;
+        float moveVertical = Input.GetAxis("Vertical") * movementSpeed;
         Debug.Log(moveHorizontal + " : " + moveVertical);
 
         this.transform.Translate(0, 0, moveVertical);
@@ -32,5 +37,28 @@ public class PlayerController : MonoBehaviour
         {
 
         }
+
+
+
+        if (this.transform.position.z > LimitZ)
+        {
+            this.transform.position = new Vector3(this.transform.position.x,this.transform.position.y,LimitZ);
+        }
+
+        if (this.transform.position.z > -LimitZ)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, LimitZ);
+        }
+
+        if (this.transform.position.z > LimitX)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, LimitX);
+        }
+
+        if (this.transform.position.z > -LimitX)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, LimitX);
+        }
+
     }
 }
