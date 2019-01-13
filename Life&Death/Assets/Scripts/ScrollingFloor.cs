2955753a20a358f99437d77608e184a9795b7687
venstex <32637535+venstex@users.noreply.github.com>
2019-01-13 17:16:47 +0100

    added mist

---
+++ Life&Death/Assets/Scripts/ScrollingFloor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingFloor : MonoBehaviour
+{
+
+    Material material;
+    [SerializeField]
+    private float speed = 5;
+    float currentscroll;
+
+    //float offset;
+    //float scrollSpeed = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        currentscroll += speed * Time.deltaTime;
+        material.mainTextureOffset = new Vector2(currentscroll, 0);
+    }
+}
